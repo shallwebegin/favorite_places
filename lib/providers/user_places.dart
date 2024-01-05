790ef12dd3bd6b3e09eccfbecdd48fc04701e77e
Berke Kartal <shallwebegin@users.noreply.github.com>
2024-01-05 16:50:26 +0300

    add CircleAvatar

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
+
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserProviderNotifier extends StateNotifier<List<Place>> {
   UserProviderNotifier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
